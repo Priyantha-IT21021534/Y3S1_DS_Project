8b821f24613ee6783115397f7f0858866dd9215c
Hesara Pasan <hesaramahendra123@gmail.com>
2023-03-19 11:37:47 +0530

    Product service backend complete

--- backend/services/products/controllers/productController.js
+++ backend/services/products/controllers/productController.js
@@ -1,15 +1,48 @@
 const Products = require("../model/products");
 
+//get all products
+const getAllProducts = async (req, res, next) => {
+  let product;
+  try {
+    product = await Products.find();
+  } catch (err) {
+    console.log(err);
+  }
+  if (!product) {
+    return res.status(404).json({ message: "Nothing found" });
+  }
+  return res.status(200).json(product);
+};
 
+//get products by id
+const getById = async (req, res, next) => {
+  const id = req.params.id;
+  let product;
+  try {
+    product = await Products.findById(id);
+  } catch (err) {
+    console.log(err);
+  }
+  if (!product) {
+    return res.status(404).json({ message: "No product found" });
+  }
+  return res.status(200).json(product);
+};
+
+//add products
 const addProduct = async (req, res, next) => {
-    const { name, brand, price } =
+    const { name, brand, price, weight, upload_date, description, image } =
       req.body;
     let product;
     try {
       product = new Products({
         name, 
         brand, 
-        price
+        price,
+        weight,
+        upload_date,
+        description,
+        image
       });
       await product.save();
     } catch (err) {
@@ -20,4 +53,50 @@ const addProduct = async (req, res, next) => {
     }
     return res.status(201).json(product);
   };
+
+  //update products
+const updateProduct = async (req, res, next) => {
+  const id = req.params.id;
+  const { name, brand, price, weight, upload_date, description, image } = req.body;
+  let product;
+  try {
+    product = await Products.findByIdAndUpdate(id, {
+      name, 
+      brand, 
+      price,
+      weight,
+      upload_date,
+      description,
+      image
+    });
+    product = await product.save();
+  } catch (err) {
+    console.log(err);
+  }
+  if (!product) {
+    return res.status(404).json({ message: "Unable to Update by id" });
+  }
+  return res.status(200).json({ product });
+};
+
+//delete products
+const deleteProduct = async (req, res, next) => {
+  const id = req.params.id;
+  let product;
+  try {
+    product = await Products.findByIdAndRemove(id);
+  } catch (err) {
+    console.log(err);
+  }
+  if (!product) {
+    return res.status(404).json({ message: "Unable to Delete by id" });
+  }
+  return res.status(200).json({ message: "Product Successfully Deleted" });
+};
+
+
   exports.addProduct = addProduct;
+  exports.getAllProducts = getAllProducts;
+  exports.getById = getById;
+  exports.updateProduct = updateProduct;
+  exports.deleteProduct = deleteProduct;
--- backend/services/products/routes/products-routs.js
+++ backend/services/products/routes/products-routs.js
@@ -6,5 +6,9 @@ const productController= require("../controllers/productController");
 
 
 router.post("/addProduct",productController.addProduct);
+router.get("/getProducts",productController.getAllProducts);
+router.get("/getId/:id",productController.getById);
+router.put("/updateProduct/:id",productController.updateProduct);
+router.delete("/deleteProduct/:id",productController.deleteProduct);
 
 module.exports = router;
