500be3237a12a8cdacb366f4c72f731532b6466a
thushanvithana <87628351+thushanvithana@users.noreply.github.com>
2023-04-10 00:07:40 +0530

    payment

---
+++ New folder/frontend/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Frame from "./components/Frame";
+
+const App = () => {
+  return (
+    <Frame />
+  );
+}
+export default App;
---
+++ backend/products.js
@@ -0,0 +1,76 @@
+const products = [
+  {
+    id: 1,
+    name: "Sidda lepa",
+    brand: "Sidda lepa",
+    desc: "test",
+    price: 85,
+    image:
+      "https://i.postimg.cc/XYgZw7ph/sale-sale-valentine-s-day-50-off.png",
+  },
+  {
+    id: 2,
+    name: "Sidda lepa ",
+    brand: "Sidda lepa",
+    desc: "test",
+    price: 699,
+    image:
+      "https://res.cloudinary.com/chaoocharles/image/upload/v1629289889/online-shop/iphone12_efhrcp.jpg",
+  },
+  {
+    id: 3,
+    name: "Sidda lepa",
+    brand: "Sidda lepa",
+    desc: "test",
+    price: 399,
+    image:
+      "https://res.cloudinary.com/chaoocharles/image/upload/v1629289889/online-shop/galaxyS_dvjf5w.png",
+  },
+  {
+    id: 3,
+    name: "Sidda lepa",
+    brand: "Sidda lepa",
+    desc: "test",
+    price: 399,
+    image:
+      "https://res.cloudinary.com/chaoocharles/image/upload/v1629289889/online-shop/galaxyS_dvjf5w.png",
+  },
+  {
+    id: 3,
+    name: "Sidda lepa",
+    brand: "Sidda lepa",
+    desc: "test",
+    price: 399,
+    image:
+      "https://res.cloudinary.com/chaoocharles/image/upload/v1629289889/online-shop/galaxyS_dvjf5w.png",
+  },
+  {
+    id: 3,
+    name: "Sidda lepa",
+    brand: "Sidda lepa",
+    desc: "test",
+    price: 399,
+    image:
+      "https://res.cloudinary.com/chaoocharles/image/upload/v1629289889/online-shop/galaxyS_dvjf5w.png",
+  },
+  {
+    id: 3,
+    name: "test",
+    brand: "test",
+    desc: "test",
+    price: 399,
+    image:
+      "https://res.cloudinary.com/chaoocharles/image/upload/v1629289889/online-shop/galaxyS_dvjf5w.png",
+  },
+  {
+    id: 3,
+    name: "test",
+    brand: "test",
+    desc: "test",
+    price: 399,
+    image:
+      "https://res.cloudinary.com/chaoocharles/image/upload/v1629289889/online-shop/galaxyS_dvjf5w.png",
+  },
+];
+
+module.exports = products;
