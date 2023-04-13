f0075e2815358339c39792d456311b950bdbccfd
IT21021534.Priyantha <IT21021534@my.sliit.lk>
2023-04-13 23:57:56 +0530

    delivery getRate endpoint complete

--- backend/services/delivery/controllers/deliveryController.js
+++ backend/services/delivery/controllers/deliveryController.js
@@ -28,6 +28,9 @@ exports.pingDeliveryServer = pingDeliveryServer;
 const getRate = async(req, res, next) => {
   const {shipfrom, shipto, weight} = req.body;
 
+  if(shipfrom == null || shipto == null || weight == null)
+    return res.status(460).json({ message: "Required data missing" });
+
   var fromCoordinates;
   var toCoordinates;
   var duration;
@@ -37,21 +40,48 @@ const getRate = async(req, res, next) => {
     const fromResults = await getCoordinates(shipfrom);
     if(fromResults.status == "ok")
       fromCoordinates = [fromResults.coordinates[0],fromResults.coordinates[1]];
+    else if(fromResults.status == "error")
+      return res.status(461).json({ message: "Store address not found" });
 
     const toResults = await getCoordinates(shipto);
     if(toResults.status == "ok")
       toCoordinates = [toResults.coordinates[0],toResults.coordinates[1]];
-
+    else if(toResults.status == "error")
+      return res.status(462).json({ message: "Client address not found" });
+      
     const distanceDuration = await getDistanceDuration(fromCoordinates, toCoordinates);
     if(distanceDuration.status == "ok"){
-      duration = distanceDuration.duration;
-      distance = distanceDuration.distance;
-    }
-    console.log(duration +", "+ distance +", "+ weight);
+      duration = Math.ceil(distanceDuration.duration / 360) / 10; // seconds -> hours
+      distance = Math.ceil(distanceDuration.distance / 1000); // meters -> km
+    } else if(distanceDuration.status == "bad")
+      return res.status(463).json({ message: "Out of range" });
+    else if(distanceDuration.status == "error")
+      return res.status(520).json({ message: distanceDuration.error });
+   
+    var rate = (duration + distance + weight * 5) * 10;
+    rate = Math.ceil((rate + 1) / 10) * 10;
+    if(rate < 100)
+      rate = 100;
 
-    //TODO: create a equation to calculate rate
+    const days = Math.ceil(duration / 24) + 1;
+
+    console.log({
+      from: fromCoordinates,
+      to: toCoordinates,
+      duration: duration,
+      distance: distance,
+      days: days,
+      rate: rate
+    })
+
+    return res.status(200).json({
+      days: days,
+      rate: rate
+    });
+    
   }catch(e){
     console.error(e);
+    return res.status(500).json({ message: "Server Error: "+ err });
   }
 
 };
@@ -88,9 +118,17 @@ async function getDistanceDuration(location1, location2){
     
     const response = await axios.post(k.DURATION_URL, data, config);
     const duration = response.data.durations[0][1];
-    const disance = response.data.distances[0][1];
+    const distance = response.data.distances[0][1];
 
-    return {"status":"ok","duration": duration, "distance": disance};
+    console.log({
+      duration: duration,
+      disance: distance
+    })
+
+    if(duration == null || distance == null)
+      return {"status":"bad","msg": "cannot be reached"};
+    else
+      return {"status":"ok","duration": duration, "distance": distance};
   }catch(e){
     console.log(e);
     return {"status":"error", "error": e};
