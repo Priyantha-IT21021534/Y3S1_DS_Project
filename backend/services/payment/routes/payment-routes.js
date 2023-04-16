faf4734edab36d1180b3238041d2e69dd2227593
IT21021534.Priyantha <IT21021534@my.sliit.lk>
2023-04-16 20:53:09 +0530

    created README.md to some services

--- backend/services/payment/routes/payment-routes.js
+++ backend/services/payment/routes/payment-routes.js
@@ -4,7 +4,6 @@ const paymentController= require("../controllers/paymentController");
 
 router.get("/", paymentController.pingPaymentServer);
 router.post("/card", paymentController.dummyCardPayment);
-// router.post("/sendSms",smsController.sendDummySms);
 
 
 module.exports = router;
